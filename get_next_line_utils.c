5439776a6c6f39adf9664a1362f24cf86ca23104
Aaron <aaronambro23@gmail.com>
2023-12-18 14:18:33 +0100

    Added: Utils with hellper functions, and get_next_line V1.0

---
+++ get_next_line_utils.c
@@ -0,0 +1,93 @@
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	int		size;
+	char	*duplicate;
+
+	i = 0;
+	size = ft_strlen(str);
+	duplicate = malloc(sizeof(char) * (size + 1));
+	if (!duplicate)
+		return (0);
+	while (str[i])
+	{
+		duplicate[i] = str[i];
+		i++;
+	}
+	duplicate[i] = '\0';
+	return (duplicate);
+}
+
+char	*ft_strchr(const char *s, int i)
+{
+	while (*s)
+	{
+		if (*s == i)
+			return ((char *)s);
+		s++;
+	}
+	if (i == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (str == 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
